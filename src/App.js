9d088d3d0a946ffc002de955d022cd1c060e3af4
Crawalrutvik <rutvikraval.crawlapps@gmail.com>
2023-12-08 12:32:33 +0530

    todo app using react js

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import "./App.css";
+import React, { useState } from "react";
+import deleticon from "../src/Assets/img/delete_icon.png";
+import editicon from "../src/Assets/img/edit_icon.png";
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [indexNum, setIndexNum] = useState(null);
+  const [Data, setData] = useState([]);
+
+  const heandelSubmit = (event) => {
+    event.preventDefault();
+    if (typeof indexNum === "number" && indexNum >= 0) {
+      setData((prevData) => {
+        const newData = [...prevData];
+        newData[indexNum] = inputValue;
+        return newData;
+      });
+      setIndexNum(null);
+    } else {
+      setData((prevData) => [...prevData, inputValue]);
+    }
+    setInputValue("");
+  };
+  const getText = (e) => {
+    setInputValue(e?.target?.value.trimStart());
+  };
+  const deleteItem = (index) => {
+    const deleteData = Data?.filter((item, Currentindex) => {
+      return Currentindex !== index;
+    });
+    setData(deleteData);
+  };
+  const editItem = (index) => {
+    const editData = Data?.filter((item, Currentindex) => {
+      return Currentindex === index;
+    });
+    setInputValue(editData[0]);
+    setIndexNum(index);
+  };
+
+  return (
+    <>
+      <div className="title">
+        <h1>To Do List</h1>
+      </div>
+      <div className="main_wrap">
+        <progress value={inputValue?.length} max={100} />
+
+        <form onSubmit={heandelSubmit}>
+          <input type="text" onChange={getText} value={inputValue} />
+          <button disabled={inputValue?.length > 0 ? false : true}>
+            submit
+          </button>
+        </form>
+        {Data?.length > 0 ? (
+          <ul className="task-list">
+            {Data.map((item, index) => (
+              <li key={index} className="task-list-item">
+                {item}
+                <div>
+                  <span onClick={() => deleteItem(index)}>
+                    <img src={deleticon} alt="delete icon" />
+                  </span>
+                  <span onClick={() => editItem(index)}>
+                    <img src={editicon} alt="edit icon" />
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div className="title">
+            <h3>plz add your to do </h3>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default App;
